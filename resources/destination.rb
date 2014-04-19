4a92683caaa0dee69d12f6a0fbf4a3a7b8a8769b
Cyrus <david@jcyr.us>
2014-04-19 18:35:06 +0800

    mark-mode

--- resources/destination.rb
+++ resources/destination.rb
@@ -4,3 +4,4 @@ default_action :add
 attribute :name, kind_of: String, required: true
 attribute :udp, kind_of: Hash, default: nil
 attribute :template, kind_of: String, default: nil
+attribute :mark_mode, kind_of: String, default: "global"
