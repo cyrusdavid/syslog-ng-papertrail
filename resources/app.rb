a724990ea7ae244d55a2a98ced8a75d44843e615
Cyrus <david@jcyr.us>
2014-04-19 17:37:43 +0800

    Fixnum

--- resources/app.rb
+++ resources/app.rb
@@ -1,5 +1,5 @@
 actions :create, :delete
 default_action :create
 
-attribute :port, kind_of: FixNum, required: true
+attribute :port, kind_of: Fixnum, required: true
 attribute :source, kind_of: Array, required: true
