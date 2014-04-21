2461dbf95cae06347bd511194694bb1f6129bdb5
Cyrus <david@jcyr.us>
2014-04-22 05:58:08 +0800

    woops, Boolean

--- resources/template.rb
+++ resources/template.rb
@@ -3,5 +3,5 @@ default_action :add
 
 attribute :template, kind_of: String, default: nil
 attribute :logentries_token, kind_of: String, default: nil
-attribute :logentries, kind_of: Boolean, default: false
-attribute :papertrail, kind_of: Boolean, default: false
+attribute :logentries, kind_of: [TrueClass, FalseClass], default: false
+attribute :papertrail, kind_of: [TrueClass, FalseClass], default: false
